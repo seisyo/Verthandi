648871663bedd2258dac3bb71426750cded85eef
seisyo <seisyo1234@gmail.com>
2015-11-28 20:29:52 +0800

    index use blade include the left_navbar & top_navbar.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>SITCON財務系統 | 首頁</title>
+
+    <link href="css/all.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <!-- Mainly scripts -->
+    <script src="js/all.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="js/plugins/pace/pace.min.js"></script>
+
+</head>
+
+<body>
+    @include('component.left_navbar')
+    @include('component.top_navbar')
+</body>
+
+</html>
