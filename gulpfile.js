bb5dc8dd4633d6068652b3c39439dfc9fa4ab5a7
seisyo <seisyo1234@gmail.com>
2015-11-28 18:32:52 +0800

    Deploy the template js, css to laravel with gulp. But there has some connect error in reading image resource.

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,29 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+    mix.styles([
+        "animate.css",
+        "bootstrap.css",
+        "style.css"
+    ], "public/css");
+
+    mix.scripts([
+        "jquery-2.1.1.js",
+        "bootstrap.js",
+        "inspinia.js",
+        "jquery-ui-1.10.4.min.js",
+        "jquery-ui.custom.min.js"
+    ], "public/js");
+
+    mix.copy("resources/assets/email_templates", "public/email_templates");
+    mix.copy("resources/assets/font-awesome", "public/font-awesome");
+    mix.copy("resources/assets/fonts", "public/fonts");
+    mix.copy("resources/img", "public/img");
+    
+    mix.copy("resources/assets/css/patterns", "public/css/patterns");
+    mix.copy("resources/assets/css/plugins", "public/css/plugins");
+    
+    mix.copy("resources/assets/js/demo", "public/js/demo");
+    mix.copy("resources/assets/js/plugins", "public/js/plugins");
 });
