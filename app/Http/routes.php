83c402de8e5c46f0c25b5c0f7d6c67af103ba1d9
seisyo <seisyo1234@gmail.com>
2015-11-28 20:19:25 +0800

    Made the left_navbar, top_navbar component.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('test');
+    return view('index');
 });
---
+++ resources/views/component/left_navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav metismenu" id="side-menu">
+            <li class="nav-header">
+                <div class="dropdown profile-element">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                        <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">Seisyo Hsu</strong>
+                         </span> <span class="text-muted text-xs block">財務組系統開發者 <b class="caret"></b></span> </span> </a>
+                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                            <li><a href="#">登出</a></li>
+                        </ul>
+                </div>
+                <div class="logo-element">
+                    IN+
+                </div>
+            </li>
+            <li class="active">
+                <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">活動管理</span><span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level collapse in">
+                    <li class="active"><a href="index.html">SITCON2016</a></li>
+                    <li><a href="">hackgen2015</a></li>
+                    <li><a href="">SITCON2015</a></li>
+                    <li><a href="">管理活動<span class="label label-primary pull-right">NEW</span></a></li>
+                </ul>
+            </li>
+            <li class="active">
+                <a href=""><i class="fa fa-th-large"></i> <span class="nav-label">報表輸出</span> </a>
+            </li>
+            <li class="active">
+                <a href=""><i class="fa fa-th-large"></i> <span class="nav-label">會計科目管理</span> </a>
+            </li>
+            <li class="active">
+                <a href=""><i class="fa fa-th-large"></i> <span class="nav-label">使用者管理</span> </a>
+            </li>
+        </ul>
+    </div>  
+</nav>
