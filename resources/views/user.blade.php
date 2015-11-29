94ae7d9cd0bd614c5b628f0c8a08f39c6cfef047
seisyo <seisyo1234@gmail.com>
2015-11-29 09:37:11 +0800

    edit & delete user also have the modal effect.

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -31,10 +31,10 @@
                     <div class="ibox float-e-margins">
                         <div class="ibox-content">
                             <div class="">
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#adduser">
                                     ＋新增使用者
                                 </button>
-                                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                <div class="modal fade" id="adduser">
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
@@ -70,8 +70,42 @@
                                         <td>seisyo-{{$i+1}}號</td>
                                         <td>1</td>
                                         <td>
-                                            <button type="button" class="btn btn-default btn-sm">編輯</button>
-                                            <button type="button" class="btn btn-danger btn-sm">刪除</button>
+                                            <button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target="#edituser">編輯</button>
+                                            <div class="modal fade" id="edituser">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                            <h4 class="modal-title" id="myModalLabel">編輯使用者</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            @include("component.modal_content")
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                                                            <button type="button" class="btn btn-success">確定修改</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteuser">刪除</button>
+                                            <div class="modal fade" id="deleteuser">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                            <h4 class="modal-title" id="myModalLabel">刪除使用者</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            確定要刪除此使用者嗎？
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                                                            <button type="button" class="btn btn-danger">確定刪除</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                 </tbody>
