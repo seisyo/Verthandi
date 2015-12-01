c56ba162c1dc80eb8086da00ca1c4a71c6f838ad
linroex <linroex@coder.tw>
2015-12-01 11:58:50 +0800

    make layout file

---
+++ resources/views/component/layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>SITCON財務系統 | @yield('title')</title>
+
+    <link href="{{url('assets/css/all.css')}}" rel="stylesheet">
+    <link href="{{url('assets/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+
+    @yield('custom_css')
+
+    <!-- Mainly scripts -->
+    <script src="{{url('assets/js/all.js')}}"></script>
+    <script src="{{url('assets/js/plugins/metisMenu/jquery.metisMenu.js')}}"></script>
+    <script src="{{url('assets/js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+    <script src="{{url('assets/js/plugins/pace/pace.min.js')}}"></script>
+
+    <!-- Custom and plugin javascript -->
+    @yield('custom_js')
+
+</head>
+
+<body class="pace-done">
+    @include('component.left_navbar')
+    <div id="page-wrapper" class="gray-bg">
+        @include('component.top_navbar')
+        @include('component.footer')
+    </div>
+</body>
+
+
+</html>
