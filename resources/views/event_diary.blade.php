031f221b541631881ead546463ea6262d9f079e4
seisyo <seisyo1234@gmail.com>
2015-12-03 12:00:10 +0800

    Almostly complete the event_diary page except edit transaction.

--- resources/views/event_diary.blade.php
+++ resources/views/event_diary.blade.php
@@ -2,21 +2,21 @@
 
 {{-- Custom css section --}}
 @section('custom_css')
-    <link rel="stylesheet" href="{{url('assets/css/bootstrap-datepicker3.css')}}">
-    <link href="{{url('assets/css/plugins/footable/footable.core.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{url('assets/css/bootstrap-datepicker3.css')}}">
+<link href="{{url('assets/css/plugins/footable/footable.core.css')}}" rel="stylesheet">
 
 @endsection
 
 {{-- Custom js section --}}
 @section('custom_js')
-    <script src="{{url('assets/js/bootstrap-datepicker.js')}}"></script>
-    <script src="{{url('assets/js/plugins/footable/footable.all.min.js')}}"></script>
-    
-    <script>
-        $(document).ready(function() {
-            $(".footable").footable();
-        });
-    </script>
+<script src="{{url('assets/js/bootstrap-datepicker.js')}}"></script>
+<script src="{{url('assets/js/plugins/footable/footable.all.min.js')}}"></script>
+
+<script>
+$(document).ready(function() {
+    $(".footable").footable();
+});
+</script>
 @endsection
 
 {{-- Sidebar default/event --}}
@@ -47,182 +47,6 @@
 @section('content')
 
 <div class="row">
-    <div class="col-md-12">
-        <table class="footable table table-stripped toggle-arrow-tiny" data-page-size="8">
-            <thead>
-                <tr>
-
-                    <th data-toggle="true">Project</th>
-                    <th>Name</th>
-                    <th>Phone</th>
-                    <th data-hide="all">Company</th>
-                    <th data-hide="all">Completed</th>
-                    <th data-hide="all">Task</th>
-                    <th data-hide="all">Date</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Project - This is example of project</td>
-                    <td>Patrick Smith</td>
-                    <td>0800 051213</td>
-                    <td>Inceptos Hymenaeos Ltd</td>
-                    <td><span class="pie">0.52/1.561</span></td>
-                    <td>20%</td>
-                    <td>Jul 14, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Alpha project</td>
-                    <td>Alice Jackson</td>
-                    <td>0500 780909</td>
-                    <td>Nec Euismod In Company</td>
-                    <td><span class="pie">6,9</span></td>
-                    <td>40%</td>
-                    <td>Jul 16, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Betha project</td>
-                    <td>John Smith</td>
-                    <td>0800 1111</td>
-                    <td>Erat Volutpat</td>
-                    <td><span class="pie">3,1</span></td>
-                    <td>75%</td>
-                    <td>Jul 18, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Gamma project</td>
-                    <td>Anna Jordan</td>
-                    <td>(016977) 0648</td>
-                    <td>Tellus Ltd</td>
-                    <td><span class="pie">4,9</span></td>
-                    <td>18%</td>
-                    <td>Jul 22, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Alpha project</td>
-                    <td>Alice Jackson</td>
-                    <td>0500 780909</td>
-                    <td>Nec Euismod In Company</td>
-                    <td><span class="pie">6,9</span></td>
-                    <td>40%</td>
-                    <td>Jul 16, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Project
-                        <small>This is example of project</small>
-                    </td>
-                    <td>Patrick Smith</td>
-                    <td>0800 051213</td>
-                    <td>Inceptos Hymenaeos Ltd</td>
-                    <td><span class="pie">0.52/1.561</span></td>
-                    <td>20%</td>
-                    <td>Jul 14, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Gamma project</td>
-                    <td>Anna Jordan</td>
-                    <td>(016977) 0648</td>
-                    <td>Tellus Ltd</td>
-                    <td><span class="pie">4,9</span></td>
-                    <td>18%</td>
-                    <td>Jul 22, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Project
-                        <small>This is example of project</small>
-                    </td>
-                    <td>Patrick Smith</td>
-                    <td>0800 051213</td>
-                    <td>Inceptos Hymenaeos Ltd</td>
-                    <td><span class="pie">0.52/1.561</span></td>
-                    <td>20%</td>
-                    <td>Jul 14, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Alpha project</td>
-                    <td>Alice Jackson</td>
-                    <td>0500 780909</td>
-                    <td>Nec Euismod In Company</td>
-                    <td><span class="pie">6,9</span></td>
-                    <td>40%</td>
-                    <td>Jul 16, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Betha project</td>
-                    <td>John Smith</td>
-                    <td>0800 1111</td>
-                    <td>Erat Volutpat</td>
-                    <td><span class="pie">3,1</span></td>
-                    <td>75%</td>
-                    <td>Jul 18, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Gamma project</td>
-                    <td>Anna Jordan</td>
-                    <td>(016977) 0648</td>
-                    <td>Tellus Ltd</td>
-                    <td><span class="pie">4,9</span></td>
-                    <td>18%</td>
-                    <td>Jul 22, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Alpha project</td>
-                    <td>Alice Jackson</td>
-                    <td>0500 780909</td>
-                    <td>Nec Euismod In Company</td>
-                    <td><span class="pie">6,9</span></td>
-                    <td>40%</td>
-                    <td>Jul 16, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Project
-                        <small>This is example of project</small>
-                    </td>
-                    <td>Patrick Smith</td>
-                    <td>0800 051213</td>
-                    <td>Inceptos Hymenaeos Ltd</td>
-                    <td><span class="pie">0.52/1.561</span></td>
-                    <td>20%</td>
-                    <td>Jul 14, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-                <tr>
-                    <td>Gamma project</td>
-                    <td>Anna Jordan</td>
-                    <td>(016977) 0648</td>
-                    <td>Tellus Ltd</td>
-                    <td><span class="pie">4,9</span></td>
-                    <td>18%</td>
-                    <td>Jul 22, 2013</td>
-                    <td><a href="#"><i class="fa fa-check text-navy"></i></a></td>
-                </tr>
-            </tbody>    
-            <tfoot>
-                <tr>
-                    <td colspan="5">
-                        <ul class="pagination pull-right"></ul>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-</div>
-
-
-{{-- <div class="row">
     <div class="col-md-12">
         <div class="ibox float-e-margins">
             <div class="ibox-title">
@@ -258,12 +82,130 @@
             <div class="ibox-content">
                 <div class="row">
                     <div class="col-md-12">
-                        
-                    </div>
-                </div> 
-            </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="footable table table-stripped toggle-arrow-tiny" data-page-size="8">
+                                    <thead>
+                                        <tr>
+                                            <th data-toggle="true">交易日期</th>
+                                            <th>記帳日</th>
+                                            <th>交易內容</th>
+                                            <th>經手人</th>
+                                            <th>記帳人</th>   
+                                            <th data-hide="all">分錄</th>
+                                            <th>備註</th>
+                                            <th>操作</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>2015-11-29</td>
+                                            <td>2015-11-30</td>
+                                            <td>行銷組購買文具</td>
+                                            <td>影子</td>
+                                            <td>遇雨</td>
+                                            <td>
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <div class="col-md-6">
+                                                            <label>借方</label>
+                                                            <div class="row">
+                                                                <div class="col-md-8">
+                                                                    <strong>文具用品</strong>
+                                                                </div>
+                                                                <div class="col-md-4">
+                                                                    <p>1000</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <label>貸方</label>
+                                                            <div class="row">
+                                                                <div class="col-md-8">
+                                                                    <strong>現金</strong>
+                                                                </div>
+                                                                <div class="col-md-4">
+                                                                    <p>800</p>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col-md-8">
+                                                                    <strong>其他應付款</strong>
+                                                                </div>
+                                                                <div class="col-md-4">
+                                                                    <p>200</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>---</td>
+                                            <td>
 
+                                                <button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target="#edittrans">編輯</button>
+
+                                                <!-- modal start -->
+                                                <div class="modal fade" id="edittrans">
+                                                    <div class="modal-dialog" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                                <h4 class="modal-title" id="myModalLabel">編輯交易分錄</h4>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <!-- @include('component.modal.transaction') -->
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                                                                <button type="button" class="btn btn-primary">確定修改</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- modal end -->
+
+                                                <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deletetrans">刪除</button>
+
+                                                <!-- modal start -->
+                                                <div class="modal fade" id="deletetrans">
+                                                    <div class="modal-dialog" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                                <h4 class="modal-title" id="myModalLabel">刪除交易分錄</h4>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                確定要刪除此交易分錄嗎？
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                                                                <button type="button" class="btn btn-danger">確定刪除</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- modal end -->
+                                            </td>
+                                        </td>
+                                    </tr>
+
+                                </tbody>    
+                                <tfoot>
+                                    <tr>
+                                        <td colspan="5">
+                                            <ul class="pagination pull-right"></ul>
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div> 
         </div>
+
     </div>
-</div> --}}
+</div>
+</div>
 @endsection
