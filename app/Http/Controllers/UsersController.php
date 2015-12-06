adaaea189debe53f2b487aeabf3c14d93a997b49
seisyo <seisyo1234@gmail.com>
2015-12-06 15:28:12 +0800

    new user can add to users table.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Users;
+use App\Users_detail;
+
+use Hash;
+use Session;
+
 class UsersController extends Controller{
 
     public function adduser(Request $request){
@@ -18,7 +24,34 @@ class UsersController extends Controller{
             'permission' => 'required|numeric|max:4|min:1'
         ]);
 
-        echo("sucess!!");
+        //產生預設密碼
+        $default_password = str_random(12);
+        
+        //將新使用者新增至users
+        $get1 = Users::create(
+        [
+            'username' => $request->get('username'),
+            'password' => Hash::make($default_password),
+            'nickname' => $request->get('nickname'),
+            'status' => 'unverified',
+            'permission' => $request->get('permission')
+        ]);
+        
+        //將使用者詳細資訊放入users_detal
+        $get2 = Users_detail::create(
+        [
+            'user_id' => Users::where('username', '=', $request->get('username'))->first()->id,
+            'first_name' => $request->get('first_name'),
+            'last_name' => $request->get('last_name'),
+            'email' => $request->get('email'),
+            'phone' => $request->get('phone')
+        ]);
+
+        //將密碼寄信給使用者
+
+        
+
+        
     }
 
     public function edituser(Request $request){
--- app/Users_detail.php
+++ app/Users_detail.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class Users_detail extends Model{
     
     protected $table = 'users_detail';
+    protected $fillable = ['user_id', 'first_name', 'last_name', 'email', 'phone'];
     protected $primaryKey  = 'user_id';
     public $timestamps = false;
 
